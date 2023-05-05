151ab94d827b44c648b7e7caeb134d555d671ea4
Blazej_Knie <bjke@gft.com>
2023-05-05 15:59:46 +0200

    Adding GoogleConfig routing example. closes #1

---
+++ src/main/java/io/github/blazejknie/msscbrewerygateway/config/GoogleConfig.java
@@ -0,0 +1,24 @@
+package io.github.blazejknie.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator myGoogleConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                      .route("google",r -> r
+                              .path("/googlesearch1")
+                                      .filters(f -> f.rewritePath("/googlesearch1(?<segment>/?.*)", "/${segment}"))
+                              .uri("https://google.com/;")
+                              )
+                      .build();
+    }
+
+}
