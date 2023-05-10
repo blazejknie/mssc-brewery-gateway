681f527ecf9efe839255f2deaa0abb8009bfd819
Blazej_Knie <bjke@gft.com>
2023-05-10 17:47:55 +0200

    configure sleuth and zipkin. Closes #7

--- src/main/java/io/github/blazejknie/msscbrewerygateway/config/LoadBalancedRoutes.java
+++ src/main/java/io/github/blazejknie/msscbrewerygateway/config/LoadBalancedRoutes.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("local_discovery")
+@Profile("localdiscovery")
 @Configuration
 public class LoadBalancedRoutes {
 
